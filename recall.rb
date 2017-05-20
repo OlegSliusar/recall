d0be3190b0a7d65333a9865ceaa97347a91046c6
Oleh Sliusar <olehsliusar@gmail.com>
2017-05-20 11:38:02 +0300

    Show notes on the home page

--- recall.rb
+++ recall.rb
@@ -19,3 +19,28 @@ get '/' do
   @title = 'All Notes'
   erb :home
 end
+
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+ 
