aceebb1d7516bda29432c69503bf0ff8eea7836b
Oleh Sliusar <olehsliusar@gmail.com>
2017-05-20 09:16:50 +0300

    Create a database schema

--- recall.rb
+++ recall.rb
@@ -1,2 +1,15 @@
 require 'sinatra'
 require 'datamapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+
+class Note
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :complete, Boolean, :required => true, :default => false
+  property :created_at, DataTime
+  property :updated_at, DataTime
+end
+
+DataMapper.finalize.auto_upgrade!
